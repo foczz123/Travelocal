b85946ad9f71899dc288a55ce35881aa8de5928e
foczz123 <foczza@gmail.com>
2022-04-03 17:51:38 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:final_project/homepage.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Travelocal',
+      home: MyHomePage(title: 'Home Page'),
+    );
+  }
+}
+
